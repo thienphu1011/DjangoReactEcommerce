0dd6f288c99d9a03a2f7fecadadd4d95e521a473
thienphu1011 <phuthien10112006@gmail.com>
2026-04-01 00:37:35 +0700

    Add: Profile and User Model

---
+++ backend/userauths/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from shortuuid.django_fields import ShortUUIDField
+
+class User(AbstractUser):
+    username= models.CharField(unique=True , max_length=100) 
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100 ,null=True ,blank=True)
+    phone =models.CharField(max_length=100 ,null=True ,blank=True)
+
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS=['username']
+
+    def __str__(self):
+        return self.email
+
+    def save(self , *args , **kwargs):
+        email_username ,mobile= self.email.split("@") # cắt email thành phần trước @ và sau @
+        if self.full_name == "" or self.full_name==None:
+            self.full_name=email_username # nếu full_name rỗng hoặc None thì gán giá trị bằng phần trước @ của email
+        if self.username == "" or self.username==None:
+            self.username=email_username # nếu username rỗng hoặc None thì gán giá trị bằng phần trước @ của email
+        super(User, self).save( *args , **kwargs) # gọi lớp cha để lưu đối tượng vào database
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    image= models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100 ,null=True ,blank=True)
+    about = models.CharField(null=True ,blank=True)
+    gender = models.CharField(max_length=100 ,null=True ,blank=True)
+    country = models.CharField(max_length=100 ,null=True ,blank=True)
+    state = models.CharField(max_length=100 ,null=True ,blank=True)
+    city = models.CharField(max_length=100 ,null=True ,blank=True)
+    address= models.CharField(max_length=100 ,null=True ,blank=True)
+    date = models.DateTimeField(auto_now_add= True)
+    pid = ShortUUIDField(unique=True,length=10, max_length= 20, alphabet="abcdefghijk") 
+
+    def __str_(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+        
+    def save(self , *args , **kwargs):
+        if self.full_name == "" or self.full_name==None:
+            self.full_name=self.user.full_name # nếu full_name rỗng hoặc None thì gán giá trị bằng full_name của user
+        super(Profile, self).save( *args , **kwargs) 
