78a5edaf201224fd0fe285848011773fab4539d6
thienphu1011 <phuthien10112006@gmail.com>
2026-08-09 00:06:38 +0700

    Merge feature

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
 from userauths import views as userauths_views
 from store import views as store_views
-<<<<<<< HEAD
-from rest_framework_simplejwt.views import TokenRefreshView
-urlpatterns = [
-     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
-     path('user/token/refresh/', TokenRefreshView.as_view()),
-     path('user/register/', userauths_views.ResgisterView.as_view()),
-=======
 from rest_framework_simplejwt.views import (   TokenRefreshView,)
 
 urlpatterns = [
@@ -15,5 +8,4 @@ urlpatterns = [
      path('user/token/refresh/', TokenRefreshView.as_view()),
      path('user/register/', userauths_views.RegisterView.as_view()),
      path('user/password-reset/<email>/', userauths_views.PasswordResetEmailVerify.as_view()),
->>>>>>> feature
 ]         
