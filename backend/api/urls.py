038f260bf145830990e23db9186e459d056ea771
thienphu1011 <phuthien10112006@gmail.com>
2026-08-27 19:04:08 +0700

    Implement create password and forgot password

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from userauths import views as userauths_views
 from store import views as store_views
-from rest_framework_simplejwt.views import (   TokenRefreshView,)
+from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
      path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
      path('user/token/refresh/', TokenRefreshView.as_view()),
      path('user/register/', userauths_views.RegisterView.as_view()),
      path('user/password-reset/<email>/', userauths_views.PasswordResetEmailVerify.as_view()),
+     path('user/password-change/', userauths_views.PasswordChangeView.as_view()),
 ]         
--- frontend/src/views/auth/CreatePassword.jsx
+++ frontend/src/views/auth/CreatePassword.jsx
@@ -1,16 +1,50 @@
 import React, { useState } from "react";
-
+import {useSearchParams,useNavigate} from 'react-router-dom';
+import apiInstance from '../../utils/axios';
 function CreatePassword() {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+    const otp= searchParams.get('otp');
+    const uidb64= searchParams.get('uidb64');
+    const handlePasswordSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            alert("Passwords do not match. Please try again.");
+            return;
+        } else {
+            const formData = new FormData();
+            formData.append('password', password);
+            formData.append('otp', otp);
+            formData.append('uidb64', uidb64);
+
+            try {
+                await apiInstance.post(`user/password-change/`, formData).then((res) => {
+                    console.log(res.data);
+                    alert("Password has been reset successfully. Please log in with your new password.");
+                    navigate('/login');
+                })}
+            
+            catch (error) {
+                alert("An error occurred while resetting the password. Please try again.");
+            }
+
+        }
+
+    }
+
+
     return (
         <div>
-            <h1>Create Password</h1>
-            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter Password' name='' id='' />
-            <br /><br />
-            <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder='Confirm Password' name='' id='' />
-            <br /><br />
-            <button>Submit</button>
+            <h1>Create New Password</h1>
+            <form onSubmit={handlePasswordSubmit}>
+                <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter New Password' name='' id='' />
+                <br /><br />
+                <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder='Confirm New Password' name='' id='' />
+                <br /><br />
+                <button type="submit">Save New Password</button>
+            </form>
         </div>
     )
 }
