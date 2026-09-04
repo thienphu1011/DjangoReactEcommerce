e99dcbd91b75401df8ac207486416615e8acd748
thienphu1011 <phuthien10112006@gmail.com>
2026-09-04 19:44:06 +0700

    Product list and detail component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import CreatePassword from './views/auth/CreatePassword';
 import StoreHeader from './views/base/StoreHeader';
 import StoreFooter from './views/base/StoreFooter';
 import MainWrapper from './layout/MainWrapper';
+import Products from './views/store/Products';
+import ProductDetail from './views/store/ProductDetail';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -20,11 +22,14 @@ function App() {
         
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Dashboard />} /> 
+        <Route path="/dashboard" element={<Dashboard />} /> 
         <Route path="/logout" element={<Logout />} />    
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/create-new-password" element={<CreatePassword />} />    
         
+        {/* Store components */}
+        <Route path="/" element={<Products />} />
+        <Route path="/detail/:slug/" element={<ProductDetail />} />
     </Routes>
     <StoreFooter />
 </BrowserRouter>
